b3cae3b86b026588e2b62e406f56b5061f976f8f
QIANQIAN SHEN <97421612+shenqq377@users.noreply.github.com>
2025-03-21 13:17:31 +0800

    Add files via upload

--- evaluation_script/__init__.py
+++ evaluation_script/__init__.py
@@ -2,6 +2,7 @@
 # Q. How to install custom python pip packages?
 
 # A. Uncomment the below code to install the custom python packages.
+"""
 
 import os
 import subprocess
@@ -13,14 +14,8 @@ def install(package):
 
     # Args:
     #     package ([str]): Package name with version
-    try:
-        subprocess.run([sys.executable,"-m","pip","install",package])
-    except subprocess.CalledProcessError as e:
-        print(f"Error occurred while installing {package}: {e.stderr}")
-    except FileNotFoundError:
-        print("Error: Pip is not found. make sure you have pip installed.")
-    except PermissionError:
-        print("Error: Permission denied. ")
+    
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 
 def install_local_package(folder_name):
@@ -29,28 +24,19 @@ def install_local_package(folder_name):
     # Args:
     #     folder_name ([str]): name of the folder placed in evaluation_script/
     
-    
-    try:
-        subprocess.run([
-            sys.executable,
-            "-m",
-            "pip",
-            "install",
-            os.path.join(str(Path(__file__).parent.absolute()), folder_name)
-        ], capture_output=True, text=True, check=True)
-        print(f"Successfully installed local package from {folder_name}.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error occurred while installing local package from {folder_name}: {e.stderr}")
-    except FileNotFoundError:
-        print("Error: Pip not found.")
-    except PermissionError:
-        print("Error: Permission denied. ")
+    subprocess.check_output(
+    [
+        sys.executable,
+        "-m",
+        "pip",
+        "install",
+        os.path.join(str(Path(__file__).parent.absolute()) + folder_name),
+    ]
+)
 
-install("shapely==1.7.1")
-install("requests==2.25.1")
+install("matplotlib")
+install("numpy")
 
-install_local_package("package_folder_name")
-
-"""
+# install_local_package("package_folder_name")
 
 from .main import evaluate
